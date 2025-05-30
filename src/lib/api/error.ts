bd9aec376354d623e7ec7d78d403d2f23cf6f600
Ongsalt <ongsa.srp.mkm@gmail.com>
2025-05-30 17:56:48 +0700

    update error message

--- src/lib/api/error.ts
+++ src/lib/api/error.ts
@@ -1,26 +1,28 @@
 
-export type APIErrorKind = "network" | "definition-changed" | "unauthorized"
+export type APIErrorKind = "network" | "definition-changed" | "unauthorized";
 
-export type ErrorKind = APIErrorKind
+export type ErrorKind = APIErrorKind;
 
 export type ErrorInfo = {
     title: string,
     description: string,
-    code?: number
-}
+    code?: number;
+};
 
 export const errorMessages: Record<ErrorKind, ErrorInfo> = {
     "definition-changed": {
-        title: "API Definitions changed",
+        title: "API Definitions mismatch",
         description: "",
         code: 418
     },
     "network": {
         title: "No internet",
         description: "Please check your internet connection and try again",
+        code: 418
     },
     "unauthorized": {
         title: "Unauthorized",
         description: "Please login",
+        code: 401
     }
-}
+};
