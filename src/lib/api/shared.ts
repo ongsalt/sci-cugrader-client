7741844983e704f50b081230b37f36fb8e40190c
Ongsalt <ongsa.srp.mkm@gmail.com>
2025-05-30 17:50:26 +0700

    update api definitions

--- src/lib/api/shared.ts
+++ src/lib/api/shared.ts
@@ -10,7 +10,7 @@ export const zStudentInfo = z.object({
     ID: z.string().refine(isNumeric),
     Name: z.string(),
     Role: z.string().refine(isNumeric), // "1" for ST:student "2" for PF (professor i think)
-})
+});
 
 export const zClassInfo = z.object({
     ClassID: z.string().refine(isNumeric),
@@ -18,8 +18,8 @@ export const zClassInfo = z.object({
     ID: z.number(),
     Instructor: z.string(),
     Section: z.string().refine(isNumeric),
-    Thumbnail: z.string().url().nullable(), // idk if this is url or not
-})
+    Thumbnail: z.string().nullable(), // ok its not
+});
 
 export function apiResult<T extends ZodType>(t: T) {
     return z.object({
@@ -32,18 +32,20 @@ export function apiResult<T extends ZodType>(t: T) {
         success: z.literal(true)
     })).transform(it => {
         if (it.success) {
-            return ok(it.data as z.infer<T>)
+            return ok(it.data as z.infer<T>);
         } else {
-            return err<never, ErrorKind>("definition-changed")
+            return err<never, ErrorKind>("definition-changed");
         }
-    })
+    });
 }
 
 export function parseApiDate(str: string) {
     // in "dd/mm/yyyy hh:mm" 
     // TODO: validate input
-    const [date, time] = str.split(" ")
-    const [day, month, year] = date.split("/").map(it => parseInt(it))
-    const [hour, minute] = time.split(":").map(it => parseInt(it))
-    return new Date(year, month - 1, day, hour, minute)
+    const [date, time] = str.split(" ");
+    const [day, month, year] = date.split("/").map(it => parseInt(it));
+    const [hour, minute] = time.split(":").map(it => parseInt(it));
+    return new Date(year, month - 1, day, hour, minute);
 }
+
+export const FALLBACK_THUMBNAIL = "https://cdn-icons-png.flaticon.com/512/3643/3643327.png";
